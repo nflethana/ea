33b263f48fc4081c0a9dffd8d6270c153233b883
Ethan Abramson <nflethana@gmail.com>
2012-08-15 17:00:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ea::Application.config.secret_token = '53db89d5b3f2dfdec5bde21300ad781e3da42885c409f8797e89d5c750b9fdc44ad8677a1b9261320becb3e6a2418a86c9b7331695536417f72a53e86b9e6265'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ea::Application.config.session_store :cookie_store, key: '_ea_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ea::Application.config.session_store :active_record_store
